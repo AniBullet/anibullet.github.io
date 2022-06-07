e0650c8fa458cd9f06e815d18adb85ad7e278cdb
AnimatorBullet <animator.bullet@gmail.com>
2022-06-08 00:28:56 +0800

    测试部署

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,29 +6,6 @@ export default defineUserConfig({
 
   dest: 'docs/.vuepress/dist',
 
-  head: [
-    [
-      'meta',
-      {
-        name: 'google-site-verification',
-        content: '_6RYTn9r5LrzCfkK1tfbs2wu0AYafihPtBJvrCm6uE4',
-      },
-    ],
-  ],
-
-  locales: {
-    '/': {
-      lang: 'zh-CN',
-      title: '测试',
-      description: '文档测试',
-    },
-    '/en/': {
-      lang: 'en-US',
-      title: 'Demo',
-      description: 'A demo site for vuepress',
-    },
-  },
-
   theme: hopeTheme({
     // 主题配置
     iconAssets: 'fontawesome',

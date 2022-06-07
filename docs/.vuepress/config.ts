59abc116a0dab2a2049ddd28b35b20e158f64bc4
AnimatorBullet <animator.bullet@gmail.com>
2022-06-07 18:40:46 +0800

    测试

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,38 +1,38 @@
-import { defineUserConfig } from "vuepress";
-import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from 'vuepress';
+import { defaultTheme } from '@vuepress/theme-default';
 
 export default defineUserConfig({
-  base: "/",
+  base: '/',
 
-  dest: "./dist",
+  dest: './dist',
 
   head: [
     [
-      "meta",
+      'meta',
       {
-        name: "google-site-verification",
-        content: "_6RYTn9r5LrzCfkK1tfbs2wu0AYafihPtBJvrCm6uE4",
+        name: 'google-site-verification',
+        content: '_6RYTn9r5LrzCfkK1tfbs2wu0AYafihPtBJvrCm6uE4',
       },
     ],
   ],
 
   locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "测试",
-      description: "文档测试",
+    '/': {
+      lang: 'zh-CN',
+      title: '测试',
+      description: '文档测试',
     },
-    "/en/": {
-      lang: "en-US",
-      title: "Demo",
-      description: "A demo site for vuepress",
+    '/en/': {
+      lang: 'en-US',
+      title: 'Demo',
+      description: 'A demo site for vuepress',
     },
   },
 
   theme: defaultTheme({
     // 主题配置
-    iconAssets: "fontawesome",
-    docsDir: "docs",
+    iconAssets: 'fontawesome',
+    docsDir: 'docs',
   }),
 
   shouldPrefetch: false,

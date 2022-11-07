f428f08461ef8f256328f1ea36839fa504b81a1f
AnimatorBullet <animator.bullet@gmail.com>
2022-11-07 17:05:12 +0800

    修复侧边栏

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -3,19 +3,18 @@ import { sidebar } from "vuepress-theme-hope";
 export const zhSidebar = sidebar({
   "/": [
     "",
-    // "slide",
-    // {
-    //   text: "如何使用",
-    //   icon: "creative",
-    //   prefix: "guide/",
-    //   link: "guide/",
-    //   children: "structure",
-    // },
     {
       text: "K帧工具_BsKeyTools",
       icon: "edit",
-      link: "tools/bskeytools/",
-      children: ["chainstools"],
+      link: "tools/",
+      children: ["bskeytools/"],
+    },
+  ],
+  "/functions/": [
+    {
+      text: "Maxscript",
+      icon: "code",
+      children: ["maxscript.md"],
     },
   ],
 });
